06dbb757c37bc09bd2c52f6a244d3d783d489dbb
DiegoVsp <diego-vespa2020@outlook.com.br>
2021-02-17 15:24:35 -0300

    att

--- js/app.js
+++ js/app.js
@@ -41,4 +41,8 @@ document.addEventListener('DOMContentLoaded', () => {
       { y: -50, opacity: 0 },
       { y: 0, opacity: 1, ease: Expo.easeInOut }, '-=0.5')
 
+      .fromTo('.hamb', 0.7,
+      { y: -50, opacity: 0 },
+      { y: 0, opacity: 1, ease: Expo.easeInOut }, '-=0.5')
+      
 })
